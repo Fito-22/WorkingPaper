18b6707c4caa9c5031f950b10f9d2cda31dd53fd
Stefanie Senger <stefanie.senger@posteo.de>
2022-09-06 13:15:10 +0200

    made biology subdiscipline mapping

--- WorkingPaper/preprocessing/utils.py
+++ WorkingPaper/preprocessing/utils.py
@@ -29,4 +29,22 @@ def make_broader_subdisciplines_physics(data: pd.DataFrame) -> pd.DataFrame:
 
     return data
 
-'''For the other disciplines the mapping dict should be written the right way around to begin with'''
+def make_broader_subdisciplines_biology(data: pd.DataFrame) -> pd.DataFrame:
+    '''The function joins all the subdisciplines from the discipline "biology" as a new column in a given DataFrame
+    and returns the modified DataFrame'''
+
+    # Make a mapping of biology subtopics from our api data to more generalised physics subtopics
+    biology_mapping_dict = {'zoology': 'zoology', 'fishery':'zoology', 'animal science':'zoology', 'oceanography': 'zoology', 'cancer research':'genetics and molecular biology', \
+        'biochemical engineering': 'genetics and molecular biology', 'genetics': 'genetics and molecular biology', 'molecular biology': 'genetics and molecular biology','biochemistry': 'genetics and molecular biology', 'chromatography': 'genetics and molecular biology', \
+        'biotechnology':'genetics and molecular biology', 'biophysics': 'genetics and molecular biology', 'horticulture': 'botany','forestry': 'botany', 'agroforestry': 'botany', \
+        'endocrinology': 'physiology', 'immunology': 'physiology', 'neuroscience': 'physiology', 'biological system': 'physiology', \
+        'cell biology': 'microbiology', 'virology': 'microbiology', 'microbiology': 'microbiology', \
+        'soil science': 'ecology', 'environmental health': 'ecology', 'environmental chemistry': 'ecology', 'ecology': 'ecology', \
+        'computational biology': 'computational biology', 'bioinformatics': 'computational biology', 'statistics': 'computational biology',
+        'database': 'computational biology', 'data mining': 'computational biology', 'world wide web': 'computational biology', 'data science': 'computational biology', 'information retrieval': 'computational biology', 'algorithm': 'computational biology', 'programming language': 'computational biology', 'artificial intelligence': 'computational biology',\
+        'evolutionary biology': 'evolutionary biology', 'botany': 'botany', 'physiology': 'physiology', 'internal medicine': 'physiology', 'anatomy': 'physiology', 'pathology':'physiology'}
+
+    # Mapping the subtopics from the mapping dict to the broader subtopics
+    data['broader_subtopic'] = data.where(data['topic'] == 'biology')['subtopic'].map(biology_mapping_dict)
+
+    return data
