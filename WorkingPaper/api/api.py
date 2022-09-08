12a8518cbf03030043506fcd2865b4cd9daaa681
Adolfo Menendez <a.menendezrua@gmail.com>
2022-09-08 15:15:35 +0200

    User Interface and api improvw

--- WorkingPaper/api/api.py
+++ WorkingPaper/api/api.py
@@ -1,14 +1,12 @@
-from distutils.log import error
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI
 from pdfminer.high_level import extract_text
 from WorkingPaper.save_load_models.load_model import load_model
 from WorkingPaper.preprocessing.preprocessing_prediction import preprocessor_pred
+from WorkingPaper.python_model_antoine.DL_logic.cleaning import final_cleaning
 from starlette.responses import Response
 import io
-import codecs
-from keras import backend as K
 import numpy as np
-
+import joblib
 
 app = FastAPI()
 
@@ -22,38 +20,40 @@ def path(path: str):
 
     print('Extracting_text...')
     text = extract_text(pdf)
+    text_clean = final_cleaning(text)
+    a=np.array([text_clean])
+    b=np.array([text_clean])
 
-    print('Preprocessing data...')
-    X_prep = preprocessor_pred(text)
-    X_pred = np.expand_dims(X_prep, axis=0)
-    print(X_prep.shape)
+    print('Loading Model...')
+    model_1 = joblib.load('/home/adolfo/code/Fito-22/WorkingPaper/WorkingPaper/local/models/models/ML_model_layer1.joblib')
+    model_2 = joblib.load('/home/adolfo/code/Fito-22/WorkingPaper/WorkingPaper/local/models/models/ML_model_layer2.joblib')
 
-    model = load_model()
-
-    print('Making the prediction...')
-    y_pred = model.predict(X_pred)
+    print('Making the first prediction...')
+    y_pred = model_1.predict(a)
     print(y_pred)
 
     print('Finding values...')
-    max_accuracy=np.max(y_pred)
-    y=list(y_pred[0])
-    i = y.index(max_accuracy)
+    i = y_pred[0]
+    print(i)
+    if i==1: topic='mathematics'
+    elif i==2: topic='physics'
+    else:
+        print('Making the second prediction...')
+        y_pred_2layer = model_2.predict(b)
+        i_2 = y_pred_2layer
+        if i_2==1: topic='medicine'
+        elif i_2==0: topic='biology'
+        else: topic='Something is wrong'
+    return {'topic':str(topic)}
 
-    if i==0: topic='biology'
-    elif i==1: topic='chemistry'
-    elif i==2: topic='medicine'
-    else: topic='psychology'
 
 
-    return {'accuracy':round(float(max_accuracy),2),'topic':str(topic)}
+#@app.post('/uploadfile')
+#async def uploadfile(file: bytes=File(...)):
 
-
-@app.post('/uploadfile')
-async def uploadfile(file: bytes=File(...)):
-
-    a = extract_text(io.BytesIO(file))
+ #   a = extract_text(io.BytesIO(file))
     #X_prep = preprocessor_pred(text)
     #model = load_model()
     #y_pred=model.predict(X_preprocess)
 
-    return a
+  #  return a
